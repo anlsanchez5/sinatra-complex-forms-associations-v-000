87a4c0aa8055150cbe26623b74b26a64c44e24da
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-04-11 00:00:20 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,7 +13,7 @@ class PetsController < ApplicationController
   post '/pets' do
     @pet = Pet.create(params[:pet])
     if params["owner"]["name"] != ""
-      @owner = Owner.create(name: params["owner"]["name"])
+      @pet.owner = Owner.create(name: params["owner"]["name"])
       @pet.owner = @owner
     end
   #  binding.pry
